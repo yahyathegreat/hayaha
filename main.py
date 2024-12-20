bff1f747b6a2655a5f146b0a1122e5eda3c9d438
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-20 20:12:05 +0500

    parrot

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ class vehicle:
 modelX = vehicle(240, 18)
 print("model max speed:", modelX.max_speed)
 print("model mileage:", modelX.mileage)
+class parrot:
+    species = "bird"
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+blu = parrot("blu", 10)
+woo = parrot("woo", 15)
+print("blu is a {}".format(blu.species))
+print("woo is also a {}".format(woo.species))
+print("{} is {} years old".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
