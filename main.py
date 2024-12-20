da2f8e9d57a53604e08d609bac8b777626159e04
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-20 19:59:14 +0500

    me sleepy

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ class student:
     grade = 10
     print("hi i am a student of grade", grade)
 ob = student
+class vehicle:
+    def ___init___(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+modelX = vehicle(240, 18)
+print("model max speed:", modelX.max_speed)
+print("model mileage:", modelX.mileage)
